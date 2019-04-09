0761abce482aa6964d8fffb4af1d01c0e68f564e
Davide De Rosa <keeshux@gmail.com>
2019-04-09 20:07:45 +0200

    Iterate over Premium and SecureCore differently
    Skip hostname for SecureCore.

--- scripts/update-servers.sh
+++ scripts/update-servers.sh
@@ -2,11 +2,14 @@
 URL="https://account.protonvpn.com/api/vpn/config?APIVersion=3&Platform=iOS&Protocol=udp&Tier=2"
 URL_PREMIUM="${URL}&Category=Server"
 URL_SECURECORE="${URL}&Category=SecureCore"
+URLS=($URL_PREMIUM $URL_SECURECORE)
+PLANS=("p" "sc")
+
 TPL="template"
 TMP="tmp"
 SERVERS_SRC="$TPL/servers.zip"
 SERVERS_DST="$TPL/servers.csv"
-SAMPLE_CFG="$TMP/at-01.protonvpn.com.udp.ovpn"
+SAMPLE_CFG="$TMP/p/at-01.protonvpn.com.udp.ovpn"
 CA="$TPL/ca.crt"
 TLS_KEY="$TPL/ta.key"
 
@@ -23,30 +26,39 @@ ID_REGEX="([a-z]+)(-free)?(-[a-z]+)?-([0-9]+)"
 mkdir -p $TPL
 rm -rf $TMP
 mkdir $TMP
-curl -L $URL_PREMIUM >$SERVERS_SRC.p
-curl -L $URL_SECURECORE >$SERVERS_SRC.sc
-unzip $SERVERS_SRC.p -d $TMP
-unzip $SERVERS_SRC.sc -d $TMP
+
+rm -f $SERVERS_DST
+for I in ${!PLANS[@]}; do
+    URL=${URLS[$I]}
+    PLAN=${PLANS[$I]}
+    SUB="$TMP/$PLAN"
+
+    curl -L $URL >$SERVERS_SRC.$PLAN
+    unzip $SERVERS_SRC.$PLAN -d $TMP/$PLAN
+
+    for CFG in `cd $SUB && ls *.ovpn`; do
+        #fr-09.protonvpn.com.udp.ovpn
+        HOST=`echo $CFG | sed -E "s/(.+)\.udp\.ovpn$/\1/"`
+        HOST_COMPS=(${HOST//./ })
+        ID=${HOST_COMPS[0]}
+        ADDRS_LIST=(`grep -E "remote ([0-9\.]+)" $SUB/$CFG | sed -E "s/^.*remote (([0-9]+\.){3}[0-9]+).*$/\1/g" | uniq`)
+        ADDRS=$(printf ":%s" "${ADDRS_LIST[@]}")
+        ADDRS=${ADDRS:1}
+
+        if [[ $ID =~ $ID_REGEX ]]; then
+            COUNTRY=${BASH_REMATCH[1]}
+            [ "${BASH_REMATCH[2]}" = "-free" ] && FREE=1 || FREE=0
+            AREA=${BASH_REMATCH[3]:1}
+            SERVER_NUM=${BASH_REMATCH[4]}
+
+            if [[ $PLAN = "sc" ]]; then
+                HOST=""
+            fi
+            echo $ID,$COUNTRY,$AREA,$SERVER_NUM,$FREE,$HOST,$ADDRS >>$SERVERS_DST
+        fi
+    done
+done
+sed -i"" -E "s/,uk,/,gb,/g" $SERVERS_DST
 
 grep -A$LINES $CA_BEGIN $SAMPLE_CFG | grep -B$LINES $CA_END | egrep -v "$CA_BEGIN|$CA_END" >$CA
 grep -A$LINES $TLS_BEGIN $SAMPLE_CFG | grep -B$LINES $TLS_END | egrep -v "$TLS_BEGIN|$TLS_END" >$TLS_KEY
-
-rm -f $SERVERS_DST
-for CFG in `cd $TMP && ls *.ovpn`; do
-    #fr-09.protonvpn.com.udp.ovpn
-    HOST=`echo $CFG | sed -E "s/(.+)\.udp\.ovpn$/\1/"`
-    HOST_COMPS=(${HOST//./ })
-    ID=${HOST_COMPS[0]}
-    ADDRS_LIST=(`grep -E "remote ([0-9\.]+)" $TMP/$CFG | sed -E "s/^.*remote (([0-9]+\.){3}[0-9]+).*$/\1/g" | uniq`)
-    ADDRS=$(printf ":%s" "${ADDRS_LIST[@]}")
-    ADDRS=${ADDRS:1}
-
-    if [[ $ID =~ $ID_REGEX ]]; then
-        COUNTRY=${BASH_REMATCH[1]}
-        [ "${BASH_REMATCH[2]}" = "-free" ] && FREE=1 || FREE=0
-        AREA=${BASH_REMATCH[3]:1}
-        SERVER_NUM=${BASH_REMATCH[4]}
-        echo $ID,$COUNTRY,$AREA,$SERVER_NUM,$FREE,$HOST,$ADDRS >>$SERVERS_DST
-    fi
-done
-sed -i"" -E "s/,uk,/,gb,/g" $SERVERS_DST
