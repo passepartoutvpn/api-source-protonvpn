609761cd08860e55eda329cdffa941d9b81db740
Davide De Rosa <keeshux@gmail.com>
2020-07-07 21:37:30 +0200

    Reuse util.rb

---
+++ scripts/util.rb
@@ -0,0 +1,17 @@
+def read_tls_wrap(strategy, dir, file, from, to)
+    lines = File.foreach(file)
+    key = ""
+    lines.with_index { |line, n|
+        next if n < from or n >= to
+        key << line.strip
+    }
+    key64 = [[key].pack("H*")].pack("m0")
+
+    return {
+        strategy: strategy,
+        key: {
+            dir: dir,
+            data: key64
+        }
+    }
+end
