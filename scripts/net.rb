2a475af89cd2e816cf9da4628899e66994002887
Davide De Rosa <keeshux@gmail.com>
2022-03-25 09:34:46 +0100

    Upgrade to v5

--- scripts/net.rb
+++ scripts/net.rb
@@ -8,99 +8,134 @@ load "util.rb"
 
 ###
 
-servers = File.foreach("../template/servers.csv")
-ca = File.read("../template/ca.crt")
-tls_wrap = read_tls_wrap("auth", 1, "../template/ta.key", 2)
+template = File.read("../template/servers.json")
+ca = File.read("../static/ca.crt")
+tls_wrap = read_tls_wrap("auth", 1, "../static/ta.key", 2)
 
 cfg = {
-    ca: ca,
-    wrap: tls_wrap,
-    ep: [
-        "UDP:80",
-        "UDP:443",
-        "UDP:4569",
-        "UDP:1194",
-        "UDP:5060",
-        "TCP:443",
-        "TCP:3389",
-        "TCP:8080",
-        "TCP:8443"
-    ],
-    cipher: "AES-256-CBC",
-    auth: "SHA512",
-    frame: 1,
-    reneg: 0,
-    eku: true,
-    random: true
-}
-
-external = {
-    hostname: "${id}.protonvpn.com"
+  ca: ca,
+  tlsWrap: tls_wrap,
+  cipher: "AES-256-CBC",
+  digest: "SHA512",
+  compressionFraming: 1,
+  renegotiatesAfterSeconds: 0,
+  checksEKU: true,
+  randomizeEndpoint: true
 }
 
 recommended = {
-    id: "default",
-    name: "Default",
-    comment: "256-bit encryption",
+  id: "default",
+  name: "Default",
+  comment: "256-bit encryption",
+  ovpn: {
     cfg: cfg,
-    external: external
+    endpoints: [
+      "UDP:80",
+      "UDP:443",
+      "UDP:4569",
+      "UDP:1194",
+      "UDP:5060",
+      "TCP:443",
+      "TCP:3389",
+      "TCP:8080",
+      "TCP:8443"
+    ]
+  }
 }
 presets = [recommended]
 
 defaults = {
-    :username => "ABCdefGH012_jklMNop34Q_R",
-    :pool => "us-free-01",
-    :preset => "default"
+  :username => "ABCdefGH012_jklMNop34Q_R",
+  :country => "US"
 }
 
 ###
 
-pools = []
-servers.with_index { |line, n|
-    id, country, extraCountry, area, num, category, hostname, resolved_joined = line.strip.split(",")
+servers = []
 
-    addresses = nil
-    if resolved_joined.nil?
-        if hostname.nil?
-            next
-        end
-        if ARGV.include? "noresolv"
-            addresses = []
-        else
-            addresses = Resolv.getaddresses(hostname)
-        end
-        addresses.map! { |a|
-            IPAddr.new(a).to_i
-        }
-    else
-        addresses = resolved_joined.split(":").map { |a|
-            IPAddr.new(a).to_i
-        }
+json = JSON.parse(template)
+json["LogicalServers"].each { |server|
+  name = server["Name"]
+  name_comps = name.split("#")
+
+  #id = server["ID"]
+  id = name
+  country = server["EntryCountry"]
+  extraCountry = server["ExitCountry"]
+  area = server["City"] || server["Region"]
+  if name_comps.size > 1
+    num = name_comps[1].to_i
+  else
+    num = nil
+  end
+  hostname = server["Domain"]
+  resolved = server["Servers"].map { |s|
+    s["EntryIP"]
+  }
+
+  if country == "UK"
+    country = "GB"
+  end
+  if extraCountry == "UK"
+    extraCountry = "GB"
+  end
+  if extraCountry == country
+    extraCountry = nil
+  end
+
+  if server["Features"].to_i & 1 == 1
+    category = "Secure Core"
+  else
+    case server["Tier"]
+    when 0
+      category = "Free"
+    when 1, 2
+      category = ""
     end
+  end
 
-    pool = {
-        :id => id,
-        :country => country.upcase
+  addresses = nil
+  if resolved.nil?
+    if hostname.nil?
+      next
+    end
+    if ARGV.include? "noresolv"
+      addresses = []
+    else
+      addresses = Resolv.getaddresses(hostname)
+    end
+    addresses.map! { |a|
+      IPAddr.new(a).to_i
     }
-    pool[:category] = category if !category.empty?
-    pool[:extra_countries] = [extraCountry.upcase] if !extraCountry.empty?
-    pool[:area] = area if !area.empty?
-    pool[:num] = num.to_i
-    if hostname.empty?
-        pool[:resolved] = true
-    else
-        pool[:hostname] = hostname
-    end
-    pool[:addrs] = addresses
-    pools << pool
+  else
+    addresses = resolved.map { |a|
+      IPAddr.new(a).to_i
+    }
+  end
+
+  server = {
+    :id => id,
+    :country => country.upcase
+  }
+  server[:category] = category if !category.empty?
+  server[:extra_countries] = [extraCountry.upcase] if !extraCountry.nil?
+  server[:area] = area if !area.nil?
+  server[:num] = num.to_i
+  if hostname.empty?
+    server[:resolved] = true
+  else
+    server[:hostname] = hostname
+  end
+  server[:addrs] = addresses
+  servers << server
 }
 
 ###
 
 infra = {
-    :pools => pools,
-    :presets => presets,
-    :defaults => defaults
+  :servers => servers,
+  :presets => presets,
+  :defaults => defaults
 }
 
 puts infra.to_json
--- update-servers.sh
+++ update-servers.sh
@@ -1,72 +1,13 @@
 #!/bin/bash
-URL="https://account.protonvpn.com/api/vpn/config?APIVersion=3&Platform=iOS&Protocol=udp&Tier=2"
-URL_PREMIUM="${URL}&Category=Server"
-URL_SECURECORE="${URL}&Category=SecureCore"
-URLS=($URL_PREMIUM $URL_SECURECORE)
-PLANS=("p" "sc")
-
+URL="https://api.protonvpn.ch/vpn/logicals"
 TPL="template"
-TMP="tmp"
-SERVERS_SRC="$TPL/servers.zip"
-SERVERS_DST="$TPL/servers.csv"
-SAMPLE_CFG="$TMP/p/at-01.protonvpn.net.udp.ovpn"
-CA="$TPL/ca.crt"
-TLS_KEY="$TPL/ta.key"
+SERVERS="$TPL/servers.json"
 
-LINES=100000
-CA_BEGIN="<ca>"
-CA_END="</ca>"
-TLS_BEGIN="<tls-auth>"
-TLS_END="</tls-auth>"
-ID_REGEX="(node|lxc)?-?([a-z]+)(-free)?(-[a-z]+)?-([0-9]+)"
-#ID_REGEX="([a-z]+)(-[a-z]+)?-([0-9]+)"
-
-# TODO: parse Tor support, grep "tor" in filename
+echo
+echo "WARNING: Certs must be updated manually!"
+echo
 
 mkdir -p $TPL
-rm -rf $TMP
-mkdir $TMP
-
-rm -f $SERVERS_DST
-for I in ${!PLANS[@]}; do
-    URL=${URLS[$I]}
-    PLAN=${PLANS[$I]}
-    SUB="$TMP/$PLAN"
-
-    if ! curl -L $URL >$SERVERS_SRC.$PLAN; then
-        exit
-    fi
-    unzip -o $SERVERS_SRC.$PLAN -d $TMP/$PLAN
-
-    for CFG in `cd $SUB && ls *.ovpn`; do
-        #fr-09.protonvpn.com.udp.ovpn
-        HOST=`echo $CFG | sed -E "s/(.+)\.udp\.ovpn$/\1/"`
-        HOST_COMPS=(${HOST//./ })
-        ID=${HOST_COMPS[0]}
-        ADDRS_LIST=(`grep -E "remote ([0-9\.]+)" $SUB/$CFG | sed -E "s/^.*remote (([0-9]+\.){3}[0-9]+).*$/\1/g" | uniq`)
-        ADDRS=$(printf ":%s" "${ADDRS_LIST[@]}")
-        ADDRS=${ADDRS:1}
-
-        if [[ $ID =~ $ID_REGEX ]]; then
-            COUNTRY=${BASH_REMATCH[2]}
-            AREA=${BASH_REMATCH[4]:1}
-            SERVER_NUM=${BASH_REMATCH[5]}
-
-            if [[ $PLAN = "sc" ]]; then
-                CATEGORY="secure core"
-                HOST=""
-                EXTRA_COUNTRY=$AREA
-                AREA=""
-            else
-                CATEGORY=""
-                [ "${BASH_REMATCH[3]}" = "-free" ] && CATEGORY="free"
-            fi
-
-            echo $ID,$COUNTRY,$EXTRA_COUNTRY,$AREA,$SERVER_NUM,$CATEGORY,$HOST,$ADDRS >>$SERVERS_DST
-        fi
-    done
-done
-sed -i"" -E "s/,uk,/,gb,/g" $SERVERS_DST
-
-grep -A$LINES $CA_BEGIN $SAMPLE_CFG | grep -B$LINES $CA_END | egrep -v "$CA_BEGIN|$CA_END" >$CA
-grep -A$LINES $TLS_BEGIN $SAMPLE_CFG | grep -B$LINES $TLS_END | egrep -v "$TLS_BEGIN|$TLS_END" >$TLS_KEY
+if ! curl -L $URL >$SERVERS; then
+    exit
+fi
